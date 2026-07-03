a46589143cf09f221dddd513f9a5508e30d7f51f
AyaRachedi <a_rachedi@estin.dz>
2026-07-03 18:25:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+ let input = document.querySelector("#input");
+            // const boutons = document.querySelectorAll("button");
+            const boutons = document.querySelectorAll("#keys button:not([id])");
+            const pourcentage = document.querySelector("#pourcentage");
+            let equal = document.querySelector("#equal");
+            let sign = document.querySelector("#sign");
+
+
+            boutons.forEach((bouton) => {
+                bouton.addEventListener("click", () => {
+                    let valeur = bouton.textContent;
+                    //  appendToDisplay(valeur);
+                    input.value += valeur;
+
+
+
+                });
+            });
+
+            equal.addEventListener("click", () => {
+                calculate();
+            });
+            clear.addEventListener("click", () => {
+                clearDisplay();
+            });
+
+            pourcentage.addEventListener("click", () => {
+                per();
+            });
+            sign.addEventListener("click", () => {
+                changSign();
+            });
+
+
+
+            function per() {
+                input.value = Number(input.value) / 100;
+            }
+            function changSign() {
+                input.value = -Number(input.value);
+            }
+            function appendToDisplay(val) {
+                input.value += val;
+            }
+            function clearDisplay() {
+                input.value = "";
+
+            }
+            function calculate() {
+                try {
+                    input.value = eval(input.value
+                        .replaceAll("×", "*")
+                        .replaceAll("÷", "/")
+                    );
+                }
+                catch (error) {
+                    input.value = "erreur";
+                }
+
+            }
+
